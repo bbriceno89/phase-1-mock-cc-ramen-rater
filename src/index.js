896b23938dca290aec9498004d98cf7758a737eb
bbriceno89 <127133827+bbriceno89@users.noreply.github.com>
2023-03-29 14:53:24 -0600

    lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-// write your code here
+// Ramen Rater Solution
+const ramenMenu = document.querySelector("#ramen-menu")
+const ramenImage = document.querySelector(".detail-image")
+const ramenName = document.querySelector(".name")
+const ramenRestaurant = document.querySelector(".restaurant")
+const ramenRating = document.querySelector("#rating-display")
+const ramenComment = document.querySelector("#comment-display")
+const newRamen = document.querySelector("#new-ramen")
+let globalRamen;
+//Challenge 1
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetchData()
+})
+//Challenge 3
+newRamen.addEventListener("submit", (e) => {
+    //e.preventDefault();
+    addNewRamen(e)
+
+})
+//fetch the ramen data
+function fetchData(){
+    fetch("http://localhost:3000/ramens")
+    .then((res)=> res.json())
+    .then((data) => {
+        data.forEach((ramen) => {
+            displayImage(ramen)
+        })
+        showDetails(data[0])
+    })
+
+}
+//Challenge 2
+function displayImage(ramen){
+    console.log(ramen)
+    let img = document.createElement("img")
+    img.src = ramen.image
+    img.id = `id${ramen.id}`
+    img.addEventListener('click', ()=> {
+        showDetails(ramen)
+        globalRamen = ramen.id
+    })
+
+    ramenMenu.appendChild(img)
+}
+function showDetails(ramen){
+    ramenImage.src = ramen.image
+    ramenName.textContent = ramen.name
+    ramenRestaurant.textContent = ramen.restaurant
+    ramenRating.textContent = ramen.rating
+    ramenComment.textContent = ramen.comment
+}
+function addNewRamen(event){
+    event.preventDefault();
+    let newObject = {
+        name: event.target["new-name"].value,
+        restaurant: event.target["new-restaurant"].value,
+        rating: event.target["new-rating"].value,
+        comment: event.target["new-comment"].value,
+        image: event.target["new-image"].value
+    }
+    displayImage(newObject)
+}
